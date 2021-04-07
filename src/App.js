1c3b2ea67dd5b0cf18ccbfcb5dcf92d6a23ff5a3
unknown <t.decarvalhosilva@gmail.com>
2021-04-07 17:53:00 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef , useState } from 'react'
 
 function App() {
+  const entrada1 = useRef()
+  const entrada2 = useRef()
+  const [numero, setNumero] = useState(null)
+
+  function clicaSomar() {
+     mensagem(parseFloat(entrada1.current.value)+parseFloat(entrada2.current.value))
+    }
+
+    function clicaSubtrair() {
+        mensagem(entrada1.current.value-entrada2.current.value)
+    }
+    function clicaMultiplicar() {
+       mensagem(entrada1.current.value*entrada2.current.value)
+    }
+    function clicaDividir() {
+        mensagem(entrada1.current.value/entrada2.current.value)
+    }
+
+    function mensagem(valor){
+      setNumero(valor)
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="numb" ClassName="input-field " ref={entrada1}  ></input> 
+      <button className="Button waves-effect waves-light btn red" onClick={clicaSomar}>+</button>
+      <button className="Button waves-effect waves-light btn" onClick={clicaSubtrair}>-</button>
+      <button className="Button waves-effect waves-light btn red" onClick={clicaMultiplicar}>*</button>
+      <button className="Button waves-effect waves-light btn" onClick={clicaDividir}>/</button>
+      <input type="numb" ClassName="input-field " ref={entrada2}  ></input> 
+
+      {numero!==null&&(<div><p>={numero.toFixed(2)}</p></div>)}  
     </div>
   );
 }
